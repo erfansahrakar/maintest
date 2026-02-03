f8da4e82f08a763381cc0976128403242be68476
erfansahrakar <erfansahrakar@gmail.com>
2026-02-04 01:51:45 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,20 @@
+"""
+Handler modules for the Telegram bot
+این فایل پوشه handlers را به یک Python package تبدیل می‌کند
+"""
+
+# Import کردن ماژول‌های handler برای دسترسی راحت‌تر
+from . import admin
+from . import user  
+from . import order
+from . import wallet_system
+from . import admin_invoice
+
+__all__ = [
+    'admin',
+    'user',
+    'order', 
+    'wallet_system',
+    'admin_invoice'
+]
 
